a476b8d06f70a2da94ff0c54ecb620e0b34efb5a
Nastya <nasty.patutina@gmail.com>
2023-05-09 14:33:49 +0300

    доделала страничку с открытием(наградой после прохождения заданий)

--- src/data/honors.js
+++ src/data/honors.js
@@ -1,22 +1,26 @@
 const honors = [
   {
     id: 1,
-    title: 'Предсказание 1',
+    title: "Открытие 1",
+    text: "Lorem ipsum dolor sit amet consectetur. Nullam senectus  sed elit  facilisis cursus  elementum ac massa. Pellentesque  dignissim dui praesent auctor leo. ",
     isOpen: true,
   },
   {
     id: 2,
-    title: 'Предсказание 2',
+    title: "Открытие 2",
+    text: "Lorem ipsum dolor sit amet consectetur. Nullam senectus  sed elit  facilisis cursus  elementum ac massa. Pellentesque  dignissim dui praesent auctor leo. ",
     isOpen: false,
   },
   {
     id: 3,
-    title: 'Предсказание 3',
+    title: "Открытие 3",
+    text: "Lorem ipsum dolor sit amet consectetur. Nullam senectus  sed elit  facilisis cursus  elementum ac massa. Pellentesque  dignissim dui praesent auctor leo. ",
     isOpen: false,
   },
   {
     id: 4,
-    title: 'Предсказание 4',
+    title: "Открытие 4",
+    text: "Lorem ipsum dolor sit amet consectetur. Nullam senectus  sed elit  facilisis cursus  elementum ac massa. Pellentesque  dignissim dui praesent auctor leo. ",
     isOpen: false,
   },
 ];
--- src/pages/HonorMessage.js
+++ src/pages/HonorMessage.js
@@ -1,5 +1,36 @@
+import styles from "./HonorMessage.module.scss";
+import Monster from "../assets/image/Monster.svg";
+import Ufo from "../assets/image/ufo.svg";
+import smallConfetti from "../assets/image/smallConfetti.svg";
+import bigConfetti from "../assets/image/bigConfetti.svg";
+import honors from "../data/honors";
+import greenMessage from "../assets/image/greenMessage.svg";
 const HonorMessage = () => {
-  return <div>HonorMessage</div>;
+  const honor = honors[0];
+  const honorTitle = honor.title;
+  const honorText = honor.text;
+
+  return (
+    <div>
+      <div className={styles.designElements}>
+        <div className={styles.containerUfoDesign}>
+          <img src={Ufo} className={styles.elementUfo} />
+          <img src={smallConfetti} className={styles.elementSmallConfetti} />
+        </div>
+        <img src={bigConfetti} className={styles.elementBigConfetti} />
+      </div>
+      <div className={styles.containerContent}>
+        <img src={Monster} />
+        <div className={styles.containerMessage}>
+          <img src={greenMessage} />
+          <div className={styles.containerMessageText}>
+            <h1 className={styles.MessageTitle}>{honorTitle}</h1>
+            <p className={styles.messageText}>{honorText}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HonorMessage;
